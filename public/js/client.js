4d61081632904ce9aa0955918746fc56c5507f27
John Chritton <jchritton33@gmail.com>
2018-05-26 23:15:53 -0500

    Change file structure, replace jQuery selectors

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,8 @@
-//Code for submit button
-
-$(document).ready(() => {
-    $("#query-form").submit((e) => {
-        e.preventDefault();
-        let currentUrl = location.href;
-        window.location.href = currentUrl + $("#time-string").val(); 
-    })
-})
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('query-form').addEventListener('submit', e => {
+    e.preventDefault();
+    const currentUrl = location.href;
+    window.location.href =
+      currentUrl + document.getElementById('time-string').value;
+  });
+});
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const index = require('./routes');
+
+const app = express();
+
+// Port setup
+const port = process.env.PORT || 8080;
+
+// Set up path for views and view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Set up directory for static served files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up index as router on root
+app.use('/', index);
+
+// Start server
+app.listen(port, () => {
+  console.log('Timestamp server is listening on port ' + port);
+});
